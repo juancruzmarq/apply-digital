466ebd65798892f96d193f2d311c10cf0cb42161
juancruzmarq <marquezjuanchy@hotmail.com>
2025-09-19 20:59:37 -0300

    feat: add ArrayOfStringValidationPipe for string array validation

---
+++ src/common/pipes/arrayOfStrings.pipe.ts
@@ -0,0 +1,18 @@
+import { BadRequestException, Injectable, PipeTransform } from "@nestjs/common";
+
+@Injectable()
+export class ArrayOfStringValidationPipe implements PipeTransform<any> {
+  transform(value: string): string[] {
+    if (!value) return []; 
+
+    const str = value.trim().split(',');
+
+    for (const s of str) {
+        if (typeof s !== 'string' || s.trim() === '') {
+            throw new BadRequestException(`Not a valid string: ${s}`);
+        }
+    }
+
+    return value.split(',').map((s) => s.trim());
+  }
+}
