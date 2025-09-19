a4e7e01526c7ca1d6576e26ca88ad37f01f166c1
juancruzmarq <marquezjuanchy@hotmail.com>
2025-09-19 12:35:53 -0300

    feat: add ApiTags and Public decorators to AppController

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,5 +1,9 @@
 import { Controller, Get } from '@nestjs/common';
+import { ApiTags } from '@nestjs/swagger';
+import { Public } from './auth/public.decorator';
 
+@ApiTags('App')
+@Public()
 @Controller()
 export class AppController {
   @Get('/health')
