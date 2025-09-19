e13d76267a7835970ff1518890f35c2bc5931b3c
juancruzmarq <marquezjuanchy@hotmail.com>
2025-09-18 22:08:26 -0300

    feat: enhance API setup with global prefix, versioning, and Swagger documentation

--- src/main.ts
+++ src/main.ts
@@ -2,11 +2,47 @@ import { NestFactory, Reflector } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ResponseTransformInterceptor } from './common/http/api-response.interceptor';
 import { AllExceptionsFilter } from './common/http/all-exceptions.filter';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import { VersioningType } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  // Global middleware
   app.useGlobalInterceptors(new ResponseTransformInterceptor(new Reflector()));
   app.useGlobalFilters(new AllExceptionsFilter());
+  app.enableCors();
+
+  app.setGlobalPrefix('api');
+
+  app.enableVersioning({
+    type: VersioningType.URI,
+    prefix: 'v',
+    defaultVersion: '1',
+  });
+
+  // Swagger setup
+  const document = SwaggerModule.createDocument(
+    app,
+    new DocumentBuilder()
+      .setTitle('Product API')
+      .setDescription('API documentation for the Product service')
+      .setVersion('1.0')
+      .addTag('products')
+      .addBearerAuth(
+        {
+          type: 'http',
+          scheme: 'bearer',
+          bearerFormat: 'JWT',
+          description: 'Enter JWT token',
+        },
+        'bearer',
+      )
+      .build(),
+  );
+  SwaggerModule.setup('api/docs', app, document, {
+    swaggerOptions: { persistAuthorization: true },
+  });
 
   await app.listen(process.env.PORT ?? 3000);
 }
