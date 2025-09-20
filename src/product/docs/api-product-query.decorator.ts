daa7cc15da4a0eab40c7d221198ea0929b0f2f22
juancruzmarq <marquezjuanchy@hotmail.com>
2025-09-19 21:36:18 -0300

    refactor: add descriptions to ApiQuery parameters in ApiAllProductsQuery decorator

--- src/product/docs/api-product-query.decorator.ts
+++ src/product/docs/api-product-query.decorator.ts
@@ -3,22 +3,21 @@ import { ApiQuery } from '@nestjs/swagger';
 
 export function ApiAllProductsQuery() {
   return applyDecorators(
-    ApiQuery({ name: 'skip', required: false, type: Number }),
-    ApiQuery({ name: 'take', required: false, type: Number }),
-    ApiQuery({ name: 'brand', required: false, type: String }),
-    ApiQuery({ name: 'model', required: false, type: String }),
-    ApiQuery({ name: 'category', required: false, type: String }),
-    ApiQuery({ name: 'type', required: false, type: String }),
+    ApiQuery({ name: 'skip', required: false, type: Number, description: 'Number of items to skip, default is 0' }),
+    ApiQuery({ name: 'take', required: false, type: Number, description: 'Number of items to take, default is 5' }),
+    ApiQuery({ name: 'brand', required: false, type: String, description: 'Comma-separated values' }),
+    ApiQuery({ name: 'model', required: false, type: String, description: 'Comma-separated values' }),
+    ApiQuery({ name: 'category', required: false, type: String, description: 'Comma-separated values'}),
     ApiQuery({ name: 'name', required: false, type: String }),
     ApiQuery({
-      name: 'colors',
+      name: 'color',
       required: false,
       type: String,
       description: 'Comma-separated values',
     }),
-    ApiQuery({ name: 'priceMin', required: false, type: Number }),
-    ApiQuery({ name: 'priceMax', required: false, type: Number }),
-    ApiQuery({ name: 'stockMin', required: false, type: Number }),
-    ApiQuery({ name: 'stockMax', required: false, type: Number }),
+    ApiQuery({ name: 'priceMin', required: false, type: Number, description: 'Minimum price filter' }),
+    ApiQuery({ name: 'priceMax', required: false, type: Number, description: 'Maximum price filter' }),
+    ApiQuery({ name: 'stockMin', required: false, type: Number, description: 'Minimum stock filter' }),
+    ApiQuery({ name: 'stockMax', required: false, type: Number, description: 'Maximum stock filter' }),
   );
 }
